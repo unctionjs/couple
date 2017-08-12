079e5570ac52e601122f38136223f70f007c18e6
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import couple from "./source.js"
+import computedProp from "./source"
 
 test(({same, end}) => {
   same(couple("a")("b"), ["a", "b"])
