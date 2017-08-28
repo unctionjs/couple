f74e711d5ef1cedca57114ee7f41f60657804319
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:50:59 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import couple from "./source"
+import couple from "./"
 
 test(({same, end}) => {
   same(couple("a")("b"), ["a", "b"])
